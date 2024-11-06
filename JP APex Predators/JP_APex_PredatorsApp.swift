2b2ac7e920e6303228afba2e4c68d30eff50fc86
Jinyuan Zhang <jinyuanzhang1992@hotmail.com>
2024-11-06 16:24:57 +0930

    Initial Commit

---
+++ JP APex Predators/JP_APex_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JP_APex_PredatorsApp.swift
+//  JP APex Predators
+//
+//  Created by Jinyuan Zhang on 06/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct JP_APex_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
